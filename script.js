a72af635eae510842d21cb3e7dbf0f1c000d0a55
Shariq Shaikh <shariqshaikh2705@gmail.com>
2025-04-27 22:01:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ let btn = document.getElementById("btn");
 btn.addEventListener("click",(event)=>{
 	event.preventDefault();
 	let speed = document.getElementById("speed").value;
+	console.log(speed);
 	let textvalue = "We love Programming";
+	let rendertext = "";
 	let text = document.getElementById("text");
 	let speedValue = 500/parseInt(speed);
-	for(let i=0;i<textvalue.length;i++){}
+	for(let i=0;i<textvalue.length;i++){
+		console.log(textvalue[i])
 		setInterval(()=>{
 			text.innerHTML+=textvalue[i];
+			console.log("Log");
 		},speedValue);
-	
 	}
+	console.log("clicked");
 })
