f70bcd7ef9362c930c06c020efe1d09d91d558bd
Shariq Shaikh <shariqshaikh2705@gmail.com>
2025-04-27 22:39:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 //your JS code here. If required.
 let speed = document.getElementById("speed");
-let speedValue = 500/1;
+let textvalue = "We love Programming";
+let text = document.getElementById("text");
+let currentIndex = 0; 
+let speedValue = 500/1; 
 speed.addEventListener("change",(event)=>{
-		speedValue = 500/parseInt(speed.value);
-	})
-		let textvalue = "We love Programming";
-		let text = document.getElementById("text");
-		
-		for(let i=0;i<textvalue.length;i++){
-			setInterval(()=>{
-				console.log(textvalue[i]); 
-				text.innerHTML+=textvalue[i];
-			},speedValue)
+	speedValue = 500/parseInt(speed.value);
+	clearInterval(window.textInterval);
+	window.textInterval = setInterval(()=>{
+		if(currentIndex<textvalue.length){
+			text.innerHTML+=textvalue[currentIndex];
+			currentIndex++;
 		}
+		else{
+			clearInterval(window.textInterval);
+		}
+	},speedValue)
+})
+		
+		
