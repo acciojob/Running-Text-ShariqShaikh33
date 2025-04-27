4f4567ce174ab4b2deec031c3c44b6a0cdf2a2bc
Shariq Shaikh <shariqshaikh2705@gmail.com>
2025-04-27 22:46:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //your JS code here. If required.
 let speed = document.getElementById("speed");
-let textvalue = "We love Programming";
+let textvalue = "We love Programming!";
 let text = document.getElementById("text");
 let currentIndex = 0; 
 let speedValue = 500/1; 
 speed.addEventListener("change",(event)=>{
 	speedValue = 500/parseInt(speed.value);
-})
+}) 
 	clearInterval(window.textInterval);
 	window.textInterval = setInterval(()=>{
 		if(currentIndex<textvalue.length){
